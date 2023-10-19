d18641e6c00ff2d41970ad537b9c87539d18c162
nmabatov <nikita-mabatov@mail.ru>
2023-10-19 13:03:55 +0300

    enhanced merge

--- src/com/mablab/data/processing/DataProcessor.java
+++ src/com/mablab/data/processing/DataProcessor.java
@@ -73,35 +73,85 @@ public class DataProcessor {
      *
      * @param fileNames      The list of sorted file names.
      * @param outputFileName The name of the output file.
-     * @throws IOException If an I/O error occurs.
      */
-    public void mergeSortedFiles(List<String> fileNames, String outputFileName) throws IOException {
-        List<BufferedReader> readers = new ArrayList<>();
-        PriorityQueue<String> heap = new PriorityQueue<>(Comparator.comparing(String::toString));
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
-            for (String fileName : fileNames) {
-                readers.add(new BufferedReader(new FileReader(fileName)));
+    public void mergeSortedFiles(List<String> fileNames, String outputFileName) {
+        try {
+            List<BufferedReader> readers = new ArrayList<>();
+            PriorityQueue<LineWrapper> minHeap = new PriorityQueue<>(Comparator.comparing(LineWrapper::getLine));
+
+            for (String inputFile : fileNames) {
+                BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                readers.add(reader);
             }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
 
             for (BufferedReader reader : readers) {
                 String line = reader.readLine();
                 if (line != null) {
-                    heap.add(line);
+                    minHeap.offer(new LineWrapper(line, reader));
                 }
             }
 
-            while (!heap.isEmpty()) {
-                String min = heap.poll();
-                writer.write(min);
+            while (!minHeap.isEmpty()) {
+                LineWrapper minLine = minHeap.poll();
+                String line = minLine.getLine();
+                BufferedReader reader = minLine.getReader();
+
+                writer.write(line);
                 writer.newLine();
 
-                for (BufferedReader reader : readers) {
-                    String line = reader.readLine();
-                    if (line != null) {
-                        heap.add(line);
-                    }
+                line = reader.readLine();
+                if (line != null) {
+                    minHeap.offer(new LineWrapper(line, reader));
                 }
             }
+
+            for (BufferedReader reader : readers) {
+                reader.close();
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    //
+    /**
+     * Helper class to wrap a line with its corresponding reader
+     */
+    private static class LineWrapper {
+        private final String line;
+        private final BufferedReader reader;
+
+        /**
+         * Constructs a LineWrapper object with the given line and reader.
+         *
+         * @param line   the line of text
+         * @param reader the BufferedReader associated with the line
+         */
+        public LineWrapper(String line, BufferedReader reader) {
+            this.line = line;
+            this.reader = reader;
+        }
+
+        /**
+         * Returns the line of text.
+         *
+         * @return the line of text
+         */
+        public String getLine() {
+            return line;
+        }
+
+        /**
+         * Returns the BufferedReader associated with the line.
+         *
+         * @return the BufferedReader associated with the line
+         */
+        public BufferedReader getReader() {
+            return reader;
+        }
+    }
+
 }
